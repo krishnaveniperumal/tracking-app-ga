b3f9aa0a2c0ad549d170b0d12d64a46ca9080b68
Krishnaveni <krishnaveni.vela@crystaldelta.com>
2021-09-24 13:53:41 +0530

    Added tracking id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 
 import React from 'react';
+import ReactGA from 'react-ga';
 import Routes from './Route'
 import NavBar from './component/Navbar'
+ReactGA.initialize('UA-208541593-1', {
+  debug: true,
+  titleCase: false,
+  gaOptions: {
+    userId: 123
+  }
+});
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     <>
